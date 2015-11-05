4f8a5c5be85a10ee328b60613e15420b6a360bb4
student1 <student1@jh-mbair-158-18.d214.org>
2015-11-05 11:01:13 -0600

    Initial Commit

---
+++ IntergalaticTravel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IntergalaticTravel
+//
+//  Created by student1 on 11/5/15.
+//  Copyright © 2015 JohnHerseyHighschool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
