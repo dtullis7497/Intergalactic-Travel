cfde71629da96a023d94e24d7827ce5e7a7081de
student1 <student1@jh-mbair-158-18.d214.org>
2015-11-06 09:48:26 -0600

    third commit

---
+++ IntergalaticTravel/Base.lproj/ImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ImageViewController.swift
+//  IntergalaticTravel
+//
+//  Created by student1 on 11/6/15.
+//  Copyright © 2015 JohnHerseyHighschool. All rights reserved.
+//
+
+import Cocoa
+
+class ImageViewController: UIViewController {
+
+}
